5dba2686288a37143213939bef679dbb8d740165
dos61831 <dos61831@CAVD650189.deltadev.ent>
2020-01-10 17:45:36 +0530

    vkakarla

--- src/main/java/com/vkakarla/springboot/demo/controller/PersonController.java
+++ src/main/java/com/vkakarla/springboot/demo/controller/PersonController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,7 +29,8 @@ public class PersonController {
 
 	}
 
-	@RequestMapping(value = "/person-management/persons", method = RequestMethod.GET)
+	//From Spring 5
+	@GetMapping(value = "/person-management/persons")
 	public ResponseEntity<Object> getPersons() {
 
 		List<Person> pList = null;
